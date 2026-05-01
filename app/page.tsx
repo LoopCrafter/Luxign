6a25300c2fe490bab3658e368be5ef02c72bd5e6
Hamed <ostovari.eng@gmail.com>
2026-05-01 13:10:58 +0200

    Fixed the issue in the github action

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="">
-      <h1>TEST</h1>
-      <Button onClick={() => redirect("/dashboard")}>CLick</Button>
-    </div>
-  );
+  redirect("/dashboard");
+  return <div className=""></div>;
 }
