42c5d64f5713ead9612a0e6eec9fb393bcd17919
Hamed <ostovari.eng@gmail.com>
2026-05-05 21:30:12 +0200

    Updated footer's Social Links

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,14 +1,27 @@
 import { Container } from "@/src/components/layout/Container";
 import Link from "next/link";
+import { Shield, FileText, Mail } from "lucide-react";
 
 const links = [
-  "Privacy Policy",
-  "Terms of Service",
-  "Contact",
-  "Instagram",
-  "Pinterest",
+  {
+    id: 1,
+    title: "Privacy Policy",
+    link: "/privacy",
+    icon: <Shield className="size-5" />,
+  },
+  {
+    id: 2,
+    title: "Terms of Service",
+    link: "/terms",
+    icon: <FileText className="size-5" />,
+  },
+  {
+    id: 3,
+    title: "Contact",
+    link: "/contact",
+    icon: <Mail className="size-5" />,
+  },
 ];
-
 export const Footer = () => (
   <footer className="border-t border-border bg-background py-10">
     <Container className="flex flex-col items-start justify-between gap-6 sm:flex-row sm:items-center">
@@ -20,9 +33,13 @@ export const Footer = () => (
       </div>
       <ul className="flex flex-wrap gap-x-6 gap-y-2 text-xs text-muted-foreground">
         {links.map((link) => (
-          <li key={link}>
-            <Link href="#" className="transition-colors hover:text-foreground">
-              {link}
+          <li key={link.id}>
+            <Link
+              href={link.link}
+              className="transition-colors hover:text-foreground flex items-center gap-1"
+            >
+              {link.icon}
+              <span className=""> {link.title}</span>
             </Link>
           </li>
         ))}
